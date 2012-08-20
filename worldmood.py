9895862e1eae7d7d552857737bd789f99262814b
dbh937 <dbh937@gmail.com>
2012-08-20 16:47:39 -0400

    began the counting function which will search for words within the query for the different moods

--- worldmood.py
+++ worldmood.py
@@ -25,7 +25,7 @@ def query():
   t = Twython()
   # Search twitter for recent tweets containing any letter in the latin alphabet
   biglist = []
-  countdown = [3,2,1]
+  countdown = [7,6,5,4,3,2,1]
   for x in countdown:
     search = t.search(q="a+OR+b+OR+c+OR+d+OR+e+OR+f+OR+g+OR+h+OR+i+OR+j+OR+k+OR+l+OR+m+OR+n+OR+o+OR+p+OR+q+OR+r+OR+s+OR+t+OR+u+OR+v+OR+w+OR+x+OR+y+OR+z",rpp=30, page = x)
     search = search['results']
@@ -35,8 +35,32 @@ def query():
     for i in search:
       if i['iso_language_code'] == 'en': # If the tweet is english
         englishlist.append(i) # add the tweet 
-    biglist.extend(englishlist) # don't want 3 lists in a list; we want one big list
-  return biglist
+    biglist.extend(englishlist) # don't want individual lists in a list; we want one big list
+  texts = []
+  for i in biglist:
+    texts.append(i['text'].lower())
+  texts = ' '.join(texts)
+  return texts
 
+def count():
+  emotions = {
+  "love": ["love"],
+  "anger": ["hate", "angry", "mad"],
+  "happiness": ["happiest", "happy", "excited", "woot", "w00t"],
+  "sadness": ["sad", "heartbroken", "upset", "depressed", "cry"],
+  "excitement": ["can't wait", "excited", "awaiting", "wait"],
+  "fear": ["scared", "terrified", "afraid", "fearful"],
+
+  "surprise": ["wow", "can't believe", "wtf", "unbelievable"], # what's the opposite of surprise?
+  "envy": ["wish", "envious", "jealous", "want", "why can't i"] # what's the opposite of envy?
+  }
+
+  opposites = [{'positive': 'love', 'negative': 'anger'}, 
+              {'positive': 'happiness', 'negative': 'sadness'}, 
+              {'positive': 'excitement', 'negative': 'fear'}]
+
+
+  return emotions
+print query()
 
       
